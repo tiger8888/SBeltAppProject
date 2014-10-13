e43be4102a89a1ddbf8da709ad3d530d47fc377c
LevinYan <342765379@qq.com>
2014-10-13 09:41:58 +0800

    测试一下commit

--- SBeltApp/AppDelegate.m
+++ SBeltApp/AppDelegate.m
@@ -13,6 +13,8 @@
 
 @implementation AppDelegate
 
+
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
